22f0a5eef1e011038babebe7e28e7c8f568efb40
NatashaRobinson23 <109933215+NatashaRobinson23@users.noreply.github.com>
2024-06-06 19:57:43 +0930

    Stored Tensors on the GPU

--- Spectrum Allocation Tool.py
+++ Spectrum Allocation Tool.py
@@ -8,15 +8,21 @@ import time
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from torch.cuda import is_available
+
+# Checking if GPU is available and creating it as a device accordingly. 
+if torch.cuda.is_available():
+    GPUdevice = torch.device("cuda")
+    ("GPU Available")
 
 # Main Class for Particle Swarm Optimization
 class ParticleSwarmOptimizer(object):
     def __init__(self):
         self.swarmSize = 10
-        self.c_cognitive = 1 # Amount the particle is influenced by the personal best. 
-        self.c_social = 1 # Amount the particle is influenced by the global best. #DEFault 2
-        self.inertiaWeight = 1 # Influence of the current velocity on its future velocity. 
-        self.velocityLimit = 0.1 # How fast the particles move within the space. 
+        self.c_cognitive = 2 # Amount the particle is influenced by the personal best. 
+        self.c_social = 2 # Amount the particle is influenced by the global best.
+        self.inertiaWeight = 0.5 # Influence of the current velocity on its future velocity. 
+        self.velocityLimit = 0.5 # How fast the particles move within the space. 
         self.maxIterations = 100 # How many times the code runs. 
         self.frequencyBounds = [np.log10(30000), np.log10(3000)] # Upper and lower frequency bounds. Default is the HF band.  
         
@@ -25,8 +31,8 @@ class ParticleSwarmOptimizer(object):
         self.fullRoleList = ['Police', 'CFS', 'Ambulance', 'Defence', 'Farmers', 'Civilians'] # CFS - Country Fire Service
         self.roleList = ['Police', 'CFS', 'Civilians']
         self.userList = [random.randint(0, len(self.roleList)-1) for temp in range(self.swarmSize)]
-        self.userTensor = torch.tensor(self.userList)
-        self.timeSlots = torch.linspace(0, 24, steps=self.swarmSize)
+        self.userTensor = torch.tensor(self.userList).cuda()
+        self.timeSlots = torch.linspace(0, 24, steps=self.swarmSize).cuda()
         self.geographical = ['Region1','Mountain','Region2']
           
     def visualise(self):
@@ -45,32 +51,26 @@ class ParticleSwarmOptimizer(object):
         
         # CONSTRAINT 1: Frequencies that can't be used eg distress beacons. 
         # Default deters away from frequencies 3500-3600.
-        restrictedPenalty = torch.zeros((self.swarmSize), dtype=torch.float)
+        restrictedPenalty = torch.zeros((self.swarmSize), dtype=torch.float).cuda()
         restrictedMask = ((frequency >= np.log10(self.forbiddenFrequencies[1])) & (frequency <= np.log10(self.forbiddenFrequencies[0])))
         restrictedPenalty[restrictedMask] = 10**10
-        print("Restricted Penalty: ", restrictedPenalty)
         
         # CONSTRANT 2: Frequency used by particular operators eg police need to talk to all other police in the area. 
         # Solution (Part 1): Proximity Evaluation. Grouped together on adjacent channels - ensures they can contact one another.  
         # Punish based on how far the particle is from the average of other particles of the same group.
-        proximityPenalty = torch.zeros((self.swarmSize), dtype=torch.float)
-        repulsionReward = torch.zeros((self.swarmSize), dtype=torch.float)
-        uniqueTypes = torch.unique(self.userTensor) # The different types possible (0 Police, 1 Civilian, 2 Fire). 
+        proximityPenalty = torch.zeros((self.swarmSize), dtype=torch.float).cuda()
+        repulsionPenalty = torch.zeros((self.swarmSize), dtype=torch.float).cuda()
+        uniqueTypes = torch.unique(self.userTensor).cuda() # The different types possible (0 Police, 1 Civilian, 2 Fire). 
 
         for index, userType in enumerate(uniqueTypes):
             frequenciesType = frequency[self.userTensor == userType] # Selects frequencies which are of the userType.            
-            typeMean = torch.mean(frequenciesType)
-            proximityPenalty[self.userTensor == userType] = torch.square(frequenciesType - typeMean) 
+            typeMean = torch.mean(frequenciesType).cuda()
+            proximityPenalty[self.userTensor == userType] = torch.square(frequenciesType - typeMean).cuda() 
                                  
-            # Print Statements
-            print("User Tensor: ", self.userTensor)
-            print("Frequencies of the Current Type: ", userType, " ", frequenciesType)
-            print("Sim Mean: ", typeMean)
-            print("Proximity Penalty current Type: ", torch.mul(proximityPenalty[self.userTensor == userType], 200))
-            print("tensor: ", torch.ones(1, self.swarmSize))
             # Appending to the graphs based on types. 
             for i in range(0, len(frequenciesType)):
-                self.graphs[index].append(frequenciesType.numpy()[i], i)    
+                
+                self.graphs[index].append(frequenciesType.cpu().numpy()[i], i)    
                 
             # Solution (Part 2): Repulsion Evaluation. Minimises interference by ensuring other groups are elsewhere. 
             # Repels certain groups from placing near one another. 
@@ -78,52 +78,21 @@ class ParticleSwarmOptimizer(object):
             for otherUserType in uniqueTypes:
                 if otherUserType != userType:
                     otherFrequencyType = frequency[self.userTensor == otherUserType] # Make a tensor for each group type. 
-                    otherTypeMean = torch.mean(otherFrequencyType) # Calculate the mean of the group type. 
-                    otherAbsDiff = torch.abs(frequenciesType - otherTypeMean) # Calculate the absolute difference between frequencies and other group mean.
-                    repulsionReward[self.userTensor == userType] += torch.ones(len(frequenciesType)) / (torch.ones(len(frequenciesType))*1.0e-10 + torch.square(otherAbsDiff)) # Append repulsion score. PSO will seek to maximise this value. 
-                    
-                    print("Diff mean: ", otherTypeMean)
-            print("Repulsion Penalty: ", repulsionReward[self.userTensor == userType])        
+                    otherTypeMean = torch.mean(otherFrequencyType).cuda() # Calculate the mean of the group type. 
+                    otherAbsDiff = torch.abs(frequenciesType - otherTypeMean).cuda() # Calculate the absolute difference between frequencies and other group mean.
+                    repulsionPenalty[self.userTensor == userType] += torch.ones(len(frequenciesType)).cuda() / (torch.ones(len(frequenciesType))).cuda() *1.0e-10 + torch.square(otherAbsDiff).cuda() # Append repulsion score. PSO will seek to maximise this value.        
             
-
-        # CONSTRAINT 3: Frequency Capabilties of the radio equipment 
-        # Interpretation: What frequencies can certain users use? What are the limitations?
-        # Possible Users: Police, CFS, Ambulance, Defence, Farmers, Civilians.
-        #   Police: Two-way radios, mobile phones, satellites. 
-        #   Country Fire Service: Two-way radios, mobile phones, satellites. 
-        #   Ambulance: Two-way radios, mobile phones, satellites. 
-        #   Defence: Two-way radios, mobile phones, satellites. 
-        #   Farmers: Two-way radios. 
-        #   Civilians: Mobile Phones. 
-        # Equipment Frequencies:    
-        #   Two-way radios: VHF, UHF.
-        #   Mobile Phones:      
-        #   Ambulances: Equipment - Two-way radios, satellite phones (UHF: 1.6GHz to 2.4GHz). Satellite - global coverage, less susceptible to terrain/atmospheric conditions.     
-        # FIRE: HF/VHF/UHF (All 3)
-        # EMERGENCY SERVICES: HF/VHF/UHF (All 3)
-        # POLICE: HF/UHF (Cannot use VHF)
-        # 
-        # 
-        #    
-        
-        # HF is more reliable at night. 
-        # Mountains can obstruct HF signals. 
-        
-
-
-        totalPenalty = restrictedPenalty + (200*proximityPenalty) + (repulsionReward) # It is trying to minimise this. 
-        print("Total Penalty: ", totalPenalty)
+        totalPenalty = restrictedPenalty + (200*proximityPenalty) + (repulsionPenalty) # It is trying to minimise this. 
         return totalPenalty
 
     def search_space(self):
-        self.upperBound = torch.tensor(self.frequencyBounds[0])
-        self.lowerBound = torch.tensor(self.frequencyBounds[1])
+        self.upperBound = torch.tensor(self.frequencyBounds[0]).cuda()
+        self.lowerBound = torch.tensor(self.frequencyBounds[1]).cuda()
         self.dimensionality = self.upperBound.size()    
 
     def populate(self):
-        self.frequency = ((self.upperBound - self.lowerBound)*torch.rand(self.swarmSize)) + self.lowerBound
-        print("starting: ", self.frequency)
-        self.velocity = (2*self.velocityLimit*torch.rand(self.swarmSize)) - self.velocityLimit
+        self.frequency = ((self.upperBound - self.lowerBound)*torch.rand(self.swarmSize).cuda()).cuda() + self.lowerBound
+        self.velocity = (2*self.velocityLimit*torch.rand(self.swarmSize).cuda()).cuda() - self.velocityLimit
         
     def minVal(self, tensor):
         minVal = tensor[0]
@@ -131,92 +100,76 @@ class ParticleSwarmOptimizer(object):
             for value in tensor:
                 if value < minVal:
                     minVal = value
-            print("function min: ", minVal)
         return float(minVal.item())
     
     def getIndex(self, value, tensor):
         for index, current in enumerate(tensor):
             if current == value:
-                print("Index: ", index)
                 return index
     
     def enforce_bounds(self):
         upperBound = self.upperBound.view(self.dimensionality,1)
         lowerBound = self.lowerBound.view(self.dimensionality,1)
-        self.frequency = torch.max(torch.min(self.frequency,upperBound),lowerBound)
-        self.velocity = torch.max(torch.min(self.velocity,torch.tensor(self.velocityLimit)),-torch.tensor(self.velocityLimit))
+        self.frequency = torch.max(torch.min(self.frequency,upperBound),lowerBound).cuda()
+        self.velocity = torch.max(torch.min(self.velocity,torch.tensor(self.velocityLimit).cuda()),-torch.tensor(self.velocityLimit).cuda())
 
     def run(self,verbosity = True):
         self.currentFitness = self.objectiveFunction(self.frequency) # Evaluates the fitness function at each particle position. 
         self.particleBest = self.frequency
         self.particleBestFitness = self.currentFitness
-        self.globalBest = torch.full((self.swarmSize,), 99.0) # Placeholder
-        self.globalBestFitness = torch.full((self.swarmSize,), 99999.0) # Placeholder
+        self.globalBest = torch.full((self.swarmSize,), 99.0).cuda() # Placeholder. High enough that it will always be replaced. 
+        self.globalBestFitness = torch.full((self.swarmSize,), 99999.0).cuda() # Placeholder. High enough that it will always be replaced. 
 
         # Multiple different swarms - global bests/fitnesses
-        for i in range(0,  len(torch.unique(self.userTensor))):
+        for i in range(0,  len(torch.unique(self.userTensor).cuda())):
             groupMask = (self.userTensor == i)
             minFitness = self.minVal(self.particleBestFitness[groupMask])
             minIndex = self.getIndex(minFitness, self.particleBestFitness[groupMask])
             minValue = self.frequency[minIndex]
             self.globalBest.masked_fill_(groupMask, minValue)
-            self.globalBestFitness.masked_fill_(groupMask, minFitness)
-        print("!!!Frequencies: ", self.frequency)
-        print("!!!Penalties: ", self.currentFitness)
-        print("!!!Global Best: ", self.globalBest)
-        print("!!!Global Best Fitness: ", self.globalBestFitness)
-        
+            self.globalBestFitness.masked_fill_(groupMask, minFitness)        
 
         for iteration in range(self.maxIterations):
             tic = time.monotonic()
-            self.velocity = ((self.inertiaWeight*self.velocity) +  # Current velocity times some weight
-                            (self.c_cognitive*torch.rand(1)*(self.particleBest-self.frequency)) + # plus how far it is from its personal best times a random number. 
-                            (self.c_social*torch.rand(1)*(self.globalBest-self.frequency)))
-            print("Velocity: ", self.velocity)
+            self.velocity = ((self.inertiaWeight*self.velocity) +  
+                            (self.c_cognitive*torch.rand(1).cuda()*(self.particleBest-self.frequency)) + 
+                            (self.c_social*torch.rand(1).cuda()*(self.globalBest-self.frequency)))
             self.frequency = self.frequency + self.velocity
             self.enforce_bounds()
             self.currentFitness = self.objectiveFunction(self.frequency) # Penalty function. 
             localMask = self.currentFitness<self.particleBestFitness # Boolean, true when current fitness less than particle best fitness. 
             self.particleBestFitness[localMask] = self.currentFitness[localMask]
-            self.particleBest[localMask] = self.frequency[localMask] # Seems fine. 
+            self.particleBest[localMask] = self.frequency[localMask]
                           
-            
             # Multiple different swarms - global bests/fitnesses
-            for i in range(0, len(torch.unique(self.userTensor))):
+            for i in range(0, len(torch.unique(self.userTensor).cuda())):
                 groupMask = (self.userTensor == i)
                 minFitness =self.minVal(self.currentFitness[groupMask])
                 minIndex = self.getIndex(minFitness, self.currentFitness[groupMask])
                 minValue = self.frequency[minIndex]
                 oldMin = self.minVal(self.globalBestFitness[groupMask])
-                print("Old Min: ", oldMin)
-                print("Fitness min: ", minFitness)
-                print("Fitness array: ", self.currentFitness[groupMask])
-                #print("Dimension: ", tempFrequency[self.currentFitness[mask].argmin()].view(self.dimensionality, 1))
+
                 if minFitness < oldMin:
                     self.globalBestFitness.masked_fill_(groupMask, minFitness)
                     self.globalBest.masked_fill_(groupMask, minValue)
                                        
-            print("!!!Frequencies: ", self.frequency)
-            print("!!!Penalties: ", self.currentFitness)
-            print("!!!Global Best: ", self.globalBest)
-            print("!!!Global Best Fitness: ", self.globalBestFitness)
-
-
-                #if self.currentFitness[mask].min() < self.globalBestFitness[mask]:
-                   # self.globalBestFitness[mask] = torch.tensor([self.currentFitness.min()]self.currentFitness[mask]
-                    #self.globalBest[mask] = temp[self.currentFitness.argmin()].view(self.dimensionality, 1)
-                    
-            
-
-           #if (self.currentFitness.min() < self.globalBestFitness):
-           #     self.globalBestFitness = self.currentFitness.min()
-           #     self.globalBest = self.frequency[self.currentFitness.argmin()].view(self.dimensionality,1)
             toc = time.monotonic()
             if (verbosity == True):
                 print('Iteration {:.0f} >> global best fitness {:.3f} | current mean fitness {:.3f} | iteration time {:.3f}'
                 .format(iteration + 1,self.globalBestFitness.min(),self.currentFitness.mean(),toc-tic))
-                print("Final Frequency: ", self.frequency)
-                
+ 
+        # Printing final group frequencies and averages.  
+        print("====================================PSO ALLOCATION OUTCOME====================================")        
+        for i in range(0, len(torch.unique(self.userTensor).cuda())):
+            print(f"Frequencies Group {i+1}: ", end='')
+            groupMask = (self.userTensor == i)
+            sum=0
+            for index, value in enumerate(self.frequency[groupMask]):
+                sum += value
+                print(f"{value:3f}    ", end='')
+            print()
+            print(f"Group {i+1} Average: {sum/(index+1):.3f}") 
+         
     def executeTool(self):
         p.initialiseGraphs()
         p.search_space()
@@ -253,14 +206,13 @@ class ScatterPlot:
 if __name__ == "__main__":
     
     # Prompting the user for input to configure the tool. 
-
     print("|===============================================================|")
     print("|Default Parameters:                                            |")
     print("|[0]  swarmSize: 10                                             |")
     print("|[1]  c_cognitive: 2                                            |")
     print("|[2]  c_social: 2                                               |")
-    print("|[3]  inertiaWeight: 0.1                                        |")
-    print("|[4]  velocityLimit: 0.1                                        |")
+    print("|[3]  inertiaWeight: 0.5                                        |")
+    print("|[4]  velocityLimit: 0.5                                        |")
     print("|[5]  maxIterations: 100                                        |")
     print("|[6]  roles (randomly assigned): Police, CFS, Civilians.        |")
     print("|[7]  timeSlots: 0-24, <swarm size> steps.                      |")
@@ -328,21 +280,4 @@ if __name__ == "__main__":
             inputBool = str(input("Do you want to change other parameters? [y/n]: "))
     
     p.executeTool()
-
-    
-
-    
-        # USE CASES:
-        # FIRE: HF/VHF/UHF
-        # EMERGENCY SERVICES: HF/VHF/UHF
-        # POLICE: HF/UHF    
-        
-        # HF is more reliable at night. 
-        # Mountains can obstruct HF signals. 
-        # 
-        # Frequency Capabilties.
-        #timeReward = torch.zeros((1, SwarmSize))     
-        #timeReliability = ((timeTensor >= 18) | (timeTensor <= 6))
-        #timeReward[timeReliability] = 1000
-        
-        #locationPenalty = torch.zeros((1, SwarmSize))         
+   
